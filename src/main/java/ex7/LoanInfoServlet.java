18f0696b053d9956a6875ed6184fe7245f6b55df
P-Pinker <pawel.pinker@gmail.com>
2018-09-04 23:08:52 +0200

    Ex 7 - loan calculator - initial commit

---
+++ src/main/java/ex7/LoanInfoServlet.java
@@ -0,0 +1,58 @@
+package ex7;
+
+import freemarker.TemplateProvider;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+
+import javax.inject.Inject;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@WebServlet("/loanInfo")
+public class LoanInfoServlet extends HttpServlet {
+
+    private final static Logger logger = Logger.getLogger(LoanInfoServlet.class.getName());
+
+    @Inject
+    private TemplateProvider templateProvider;
+
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        resp.setContentType("text/html;charset=UTF-8");
+        Template template = templateProvider.getTemplate(getServletContext(), "LoanInput.ftlh");
+        Map<String, Object> model = new HashMap<>();
+
+        try {
+            template.process(model, resp.getWriter());
+        } catch (TemplateException e) {
+            logger.log(Level.SEVERE, e.getMessage());
+        }
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        resp.setContentType("text/html;charset=UTF-8");
+        Template template = templateProvider.getTemplate(getServletContext(), "LoanOutput.ftlh");
+        Map<String, Object> model = new HashMap<>();
+
+        try {
+            template.process(model, resp.getWriter());
+        } catch (TemplateException e) {
+            logger.log(Level.SEVERE, e.getMessage());
+        }
+
+    }
+
+}
---
+++ src/main/java/ex7/Model.java
@@ -0,0 +1,12 @@
+package ex7;
+
+import java.math.BigDecimal;
+
+public class Model {
+
+    BigDecimal loan;
+    Integer months;
+    Double profit;
+    Double commission;
+
+}
