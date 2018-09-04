32b4de1aac7b60b2b797a33282fb6dd18a4c8cb5
P-Pinker <pawel.pinker@gmail.com>
2018-09-04 23:58:52 +0200

    Added templates and refactored model class

--- src/main/java/ex7/Model.java
+++ src/main/java/ex7/Model.java
@@ -4,9 +4,47 @@ import java.math.BigDecimal;
 
 public class Model {
 
-    BigDecimal loan;
-    Integer months;
-    Double profit;
-    Double commission;
+    private BigDecimal loan;
+    private Integer months;
+    private Double profit;
+    private Double commission;
 
+    public Model(BigDecimal loan, Integer months, Double profit, Double commission) {
+        this.loan = loan;
+        this.months = months;
+        this.profit = profit;
+        this.commission = commission;
+    }
+
+    public BigDecimal getLoan() {
+        return loan;
+    }
+
+    public void setLoan(BigDecimal loan) {
+        this.loan = loan;
+    }
+
+    public Integer getMonths() {
+        return months;
+    }
+
+    public void setMonths(Integer months) {
+        this.months = months;
+    }
+
+    public Double getProfit() {
+        return profit;
+    }
+
+    public void setProfit(Double profit) {
+        this.profit = profit;
+    }
+
+    public Double getCommission() {
+        return commission;
+    }
+
+    public void setCommission(Double commission) {
+        this.commission = commission;
+    }
 }
