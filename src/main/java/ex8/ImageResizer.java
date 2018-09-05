ae08151af696a57f8dd9a4fdc3d350237c0b831c
P-Pinker <pawel.pinker@gmail.com>
2018-09-05 22:33:48 +0200

    Ex 8 - resizing pics - done

--- src/main/java/ex8/ImageResizer.java
+++ src/main/java/ex8/ImageResizer.java
@@ -3,23 +3,16 @@ package ex8;
 import ex7.LoanInfoServlet;
 
 import javax.imageio.ImageIO;
-import java.awt.*;
+import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.util.Scanner;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class ImageResizer {
 
     private final static Logger logger = Logger.getLogger(LoanInfoServlet.class.getName());
-
-//    Scanner scanner = new Scanner(System.in);
-//
-//    public String getSourceImagePath() {
-//        String sourceImagePath = scanner.nextLine();
-//        return sourceImagePath;
-//    }
 
     public static void resize (String sourceImagePath, String resizedImagePath) throws IOException {
 
@@ -38,7 +31,20 @@ public class ImageResizer {
         String formatName = resizedImagePath.substring(resizedImagePath.lastIndexOf(".") + 1);
 
         ImageIO.write(resizedImage, formatName, new File(resizedImagePath));
-        
+
+    }
+
+    public static void main(String[] args) {
+
+        String sourceImagePath1 = "C:\\Users\\ppinker\\Downloads\\IMG_20180820_142034.jpg";
+        String resizedImagePath1 = "C:\\Users\\ppinker\\Downloads\\IMG_20180820_142034-small.jpg";
+
+        try {
+            ImageResizer.resize(sourceImagePath1, resizedImagePath1);
+        } catch (IOException e) {
+            System.out.println("Wystąpił błąd.");
+            logger.log(Level.SEVERE, e.getMessage());
+        }
     }
 
 }
