0ce67eb512fe46a01081697785878adbf5a2629a
P-Pinker <pawel.pinker@gmail.com>
2018-09-13 04:34:37 +0200

    Ex 10 initial commit

---
+++ src/main/java/ex10/UnitConverter.java
@@ -0,0 +1,16 @@
+package ex10;
+
+public interface UnitConverter {
+
+    double celsiusToFahrenheit(double c);
+
+    double fahrenheitToCelsius(double f);
+
+    double milesToKilometers(double m);
+
+    double kilometersToMiles(double k);
+
+    double poundsToKilograms(double p);
+
+    double kilogramsToPounds(double k);
+}
---
+++ src/main/java/ex10/UnitConverterImpl.java
@@ -0,0 +1,36 @@
+package ex10;
+
+public class UnitConverterImpl implements UnitConverter {
+
+
+    @Override
+    public double celsiusToFahrenheit(double c) {
+        return 0;
+    }
+
+    @Override
+    public double fahrenheitToCelsius(double f) {
+        return 0;
+    }
+
+    @Override
+    public double milesToKilometers(double m) {
+        return 0;
+    }
+
+    @Override
+    public double kilometersToMiles(double k) {
+        return 0;
+    }
+
+    @Override
+    public double poundsToKilograms(double p) {
+        return 0;
+    }
+
+    @Override
+    public double kilogramsToPounds(double k) {
+        return 0;
+    }
+
+}
